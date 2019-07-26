fd54c87c8089ebad6f87b271ae91fc717a70f4f0
VINCENT HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGVI7@OOCL.COM>
2019-07-26 08:20:19 +0800

    [HGC]:delete dead code

--- src/views/AppointmentTime.vue
+++ src/views/AppointmentTime.vue
@@ -1,96 +1,3 @@
-<!--<template>-->
-  <!--<a-form :form="form">-->
-    <!--<a-form-item-->
-      <!--:label-col="formItemLayout.labelCol"-->
-      <!--:wrapper-col="formItemLayout.wrapperCol"-->
-      <!--label="Name"-->
-    <!--&gt;-->
-      <!--<a-input-->
-        <!--v-decorator="[-->
-          <!--'waybillNumber',-->
-          <!--{rules: [{ required: true, message: '请输入你的运单号' }]}-->
-        <!--]"-->
-        <!--placeholder="请输入你的运单号"-->
-      <!--/>-->
-    <!--</a-form-item>-->
-    <!--<a-form-item-->
-      <!--:label-col="formItemLayout.labelCol"-->
-      <!--:wrapper-col="formItemLayout.wrapperCol"-->
-      <!--label="取件时间"-->
-    <!--&gt;-->
-      <!--<a-time-picker use12Hours format="h:mm a" @change="onChange" />-->
-    <!--</a-form-item>-->
-    <!--<a-form-item-->
-      <!--:label-col="formTailLayout.labelCol"-->
-      <!--:wrapper-col="formTailLayout.wrapperCol"-->
-    <!--&gt;-->
-      <!--<a-checkbox-->
-        <!--:checked="checkNick"-->
-        <!--@change="handleChange"-->
-      <!--&gt;-->
-        <!--Nickname is required-->
-      <!--</a-checkbox>-->
-    <!--</a-form-item>-->
-    <!--<a-form-item-->
-      <!--:label-col="formTailLayout.labelCol"-->
-      <!--:wrapper-col="formTailLayout.wrapperCol"-->
-    <!--&gt;-->
-      <!--<a-button-->
-        <!--type="primary"-->
-        <!--@click="check"-->
-      <!--&gt;-->
-        <!--Check-->
-      <!--</a-button>-->
-    <!--</a-form-item>-->
-  <!--</a-form>-->
-<!--</template>-->
-
-<!--<script>-->
-  <!--const formItemLayout = {-->
-    <!--labelCol: { span: 4 },-->
-    <!--wrapperCol: { span: 8 },-->
-  <!--};-->
-  <!--const formTailLayout = {-->
-    <!--labelCol: { span: 4 },-->
-    <!--wrapperCol: { span: 8, offset: 4 },-->
-  <!--};-->
-  <!--export default {-->
-    <!--data () {-->
-      <!--return {-->
-        <!--time:'',-->
-        <!--checkNick: false,-->
-        <!--formItemLayout,-->
-        <!--formTailLayout,-->
-        <!--form: this.$form.createForm(this),-->
-      <!--};-->
-    <!--},-->
-    <!--methods: {-->
-      <!--check  () {-->
-        <!--this.form.validateFields(-->
-          <!--(err) => {-->
-            <!--if (!err) {-->
-              <!--console.info('success');-->
-            <!--}-->
-          <!--},-->
-        <!--);-->
-      <!--},-->
-      <!--handleChange  (e) {-->
-        <!--this.checkNick = e.target.checked;-->
-        <!--this.$nextTick(() => {-->
-          <!--this.form.validateFields(['nickname'], { force: true });-->
-        <!--});-->
-      <!--},-->
-      <!--onChange(time, timeString){-->
-        <!--this.time = time-->
-      <!--},-->
-    <!--},-->
-  <!--};-->
-<!--</script>-->
-
-<!--<style scoped>-->
-
-<!--</style>-->
-
 <template>
   <div>
     <p>运单号：<input type="text" v-model="orderId"></p>
@@ -98,8 +5,8 @@
     <a-date-picker
       format="YYYY-MM-DD" @change="getChange"/>
     <a-time-picker use24Hours @change="onChange"/>
-    <button @click="bookFetch">确定</button>
-    <button @click="goMain">取消</button>
+    <button @click="confirm">确定</button>
+    <button @click="cancel">取消</button>
 
   </div>
 </template>
@@ -121,7 +28,7 @@
       onChange (time,timeString) {
         this.timeString = timeString
       },
-      bookFetch(){
+      confirm(){
         let pkg = {
           id : this.orderId,
           time : `${this.date} ${this.timeString} `
@@ -129,7 +36,7 @@
         this.$store.dispatch('updateParcelByPost',pkg)
         this.$router.push("/")
       },
-      goMain(){
+      cancel(){
         this.$router.push("/")
       }
     }
